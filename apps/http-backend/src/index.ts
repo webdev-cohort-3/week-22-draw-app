6d083762873245b26f711c415b49170f74738635
Rahul NS <rahul9380767545@gmail.com>
2025-04-09 20:55:18 +0530

    add zod validation to signin route

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
-import { CreateUserSchema } from "@repo/common/types";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
 
 const app = express();
 
@@ -30,8 +30,13 @@ app.post("/signup", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        });
+        return;
+    }
 
     const userId = 1;
     const token = jwt.sign({
@@ -41,8 +46,8 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/room", middleware, (req, res) => {
-    // db call
 
+    // db call
     res.json({
         roomId: 123
     });
